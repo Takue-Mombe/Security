1d364f62df75ec1a9da269fb3edbe5bf020ef02b
mombe <h220462g@hit.ac.zw>
2024-03-16 23:20:22 +0200

    had to re-do the whole system

--- src/main/java/org/projecth200/camsh200/Repositories/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.projecth200.camsh200.Repositories;
-
-import org.projecth200.camsh200.Models.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,String> {
-}
--- src/main/java/org/projecth200/camsh200/Repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.projecth200.camsh200.Repositories;
-
-import org.projecth200.camsh200.Models.user;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<user,String > {
-}
