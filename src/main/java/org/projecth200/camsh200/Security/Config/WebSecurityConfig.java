d9b1121c1bdf71e598264d7408fad079689c1cb4
mombe <h220462g@hit.ac.zw>
2024-03-17 00:08:30 +0200

    turns out WebSecurityConfigurer is deprecated in latest spring app; need to look into alternatives will leave it here unless then i can fi it or create a new project

--- src/main/java/org/projecth200/camsh200/Security/Config/WebSecurityConfig.java
+++ src/main/java/org/projecth200/camsh200/Security/Config/WebSecurityConfig.java
@@ -1,4 +1,12 @@
 package org.projecth200.camsh200.Security.Config;
 
-public class WebSecurityConfig {
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 }
